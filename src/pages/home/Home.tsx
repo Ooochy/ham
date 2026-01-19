8b59b4596aa5b1f1de246f5bf9746d06aeca4ea3
cuihonghyu1 <cuihongyu1@guazi.com>
2026-01-19 17:38:13 +0800

    feat: 给题库一个入口

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom'
+
 export default function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1>网站施工中</h1>
+      <p>敬请期待！</p>
+      <Link to="/ham">跳转到业余电台操作证书考试题库</Link>
     </div>
   )
 }
